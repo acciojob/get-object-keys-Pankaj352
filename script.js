d02395578735fb1e682f8868c9d1b2c445227158
Pankaj Maurya <100466858+Pankaj352@users.noreply.github.com>
2025-02-08 22:18:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,14 @@
 //your JS code here. If required.
-const student{
-	name:"",
+const student = {
+  name: ""
+};
+
+function getKeys(obj) {
+  return Object.keys(obj);
 }
 
-function getKeys(obj){
-	return Object.keys(obj);
-}
+console.log(getKeys(student)); // Output: ["name"]
 
-console.log(getKeys(student));
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
