17736921e5dff90573c32ec9a8ff83798b529de1
Pankaj Maurya <100466858+Pankaj352@users.noreply.github.com>
2025-02-11 11:12:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ function getKeys(obj) {
   return Object.keys(obj);
 }
 
-console.log(getKeys(student)); // Output: ["name"]
+// console.log(getKeys(student));
 
 Object.prototype.getKeys = function() {
   return Object.keys(this);
