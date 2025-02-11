1540eba1531514d080674482c95f7dcfc9f20714
Pankaj Maurya <100466858+Pankaj352@users.noreply.github.com>
2025-02-11 11:18:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
-const student = { name: "" };
+//your JS code here. If required.
+const student = {
+  name: ""
+};
 
 function getKeys(obj) {
   return Object.keys(obj);
 }
 
-console.log(getKeys(student));
+// console.log(getKeys(student));
+
+Object.prototype.getKeys = function() {
+  return Object.keys(this);
+};
